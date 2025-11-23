6f18e9f76739b81f784fe135c9f26a1f887b3dab
8230148 <8230148@estg.ipp.pt>
2025-11-23 19:28:46 +0000

    alteracoes finais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
+import { PlayerListPageComponent } from './components/pages/player-list/player-list-page.component';
+import { PlayerProfilePageComponent } from './components/pages/player-profile/player-profile-page.component';
+import { SettingsPageComponent } from './components/pages/settings-page/settings-page.component';
+import { PlayerProfileRedirectComponent } from './components/pages/player-profile/player-profile-redirect/player-profile-redirect.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
+import { LogoutComponent } from './components/pages/login/logout.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
@@ -7,4 +15,41 @@ export const routes: Routes = [
     // {path: '', component: Profile},
     // {path: 'login', component: Login},
     // {path: 'register', component: Register},
+    {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'players',
+    component: PlayerListPageComponent,
+    canActivate: [AuthGuard],
+    data: { isAdmin: true }
+  },
+  {
+    path: 'players/me',
+    component: PlayerProfileRedirectComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'players/details/:playerId',
+    component: PlayerProfilePageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'settings',
+    component: SettingsPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'players',
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from './components/partials/header/header';
 import { TeamService } from './services/team.service';
 import { Footer } from './components/partials/footer/footer';
+import { SidebarComponent } from './components/partials/nav/sidebar/sidebar.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, SidebarComponent, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  standalone: true,
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useValue: authInterceptor, multi: true },
+  ]
 })
 export class App {
   protected readonly title = signal('frontend');
