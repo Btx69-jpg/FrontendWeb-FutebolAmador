1d50bcf4c65b29ee0de2a8ccab3d0c8a460b00e0
IanDCosta <8220005@estg.ipp.pt>
2025-11-24 13:27:03 +0000

    fix paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,66 +12,21 @@ import { PlayerMembershipRequestsPageComponent } from './components/pages/member
 import { TeamMembersPageComponent } from './components/pages/team-members/team-members.component';
 import { TeamMembershipRequestsPageComponent } from './components/pages/membership-requests/team-membership-requests.component';
 
-
 export const routes: Routes = [
-    {path: '', component: Home},
-    // {path: '', component: Team},
-    // {path: '', component: Profile},
-    // {path: 'login', component: Login},
-    // {path: 'register', component: Register},
-    {
-    path: 'signup',
-    component: SignupComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'logout',
-    component: LogoutComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'players',
-    component: PlayerListPageComponent,
-    canActivate: [AuthGuard],
-    //data: { isAdmin: true }
-  },
-  {
-    path: 'players/me',
-    component: PlayerProfileRedirectComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'players/details/:playerId',
-    component: PlayerProfilePageComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'players/membership-requests',
-    component: PlayerMembershipRequestsPageComponent,
-    canActivate: [AuthGuard],
-    data: { requiresTeam: false },
-  },
-  {
-    path: 'team/members',
-    component: TeamMembersPageComponent,
-    canActivate: [AuthGuard],
-    //data: { isAdmin: true, requiresTeam: true },
-  },
-  {
-    path: 'team/membership-requests',
-    component: TeamMembershipRequestsPageComponent,
-    canActivate: [AuthGuard],
-    //data: { requiresTeam: true },
-  },
-  {
-    path: 'settings',
-    component: SettingsPageComponent,
-  },
-  {
-    path: '**',
-    redirectTo: 'players',
-  },
+  { path: '', component: Home },
+  // {path: '', component: Team},
+  // {path: '', component: Profile},
+  // {path: 'login', component: Login},
+  // {path: 'register', component: Register},
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent, canActivate: [AuthGuard]},
+  { path: 'players', component: PlayerListPageComponent, canActivate: [AuthGuard], /*data: { isAdmin: true }*/ },
+  { path: 'players/me', component: PlayerProfileRedirectComponent, canActivate: [AuthGuard]},
+  { path: 'players/details/:playerId', component: PlayerProfilePageComponent, canActivate: [AuthGuard]},
+  { path: 'players/membership-requests', component: PlayerMembershipRequestsPageComponent, canActivate: [AuthGuard], data: { requiresTeam: false },},
+  { path: 'team/members', component: TeamMembersPageComponent, canActivate: [AuthGuard], /*data: { isAdmin: true, requiresTeam: true },*/},
+  { path: 'team/membership-requests', component: TeamMembershipRequestsPageComponent, canActivate: [AuthGuard], /*data: { requiresTeam: true },*/},
+  { path: 'settings', component: SettingsPageComponent},
+  { path: '**', redirectTo: 'players'},
 ];
