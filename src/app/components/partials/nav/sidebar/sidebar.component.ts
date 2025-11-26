cb203f372f0c04800341e570e6f3a3460da823d5
8230148 <8230148@estg.ipp.pt>
2025-11-26 14:09:02 +0000

    documentacao sidebar

--- src/app/components/partials/nav/sidebar/sidebar.component.ts
+++ src/app/components/partials/nav/sidebar/sidebar.component.ts
@@ -3,6 +3,10 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../..//services/auth.service';
 
+/**
+ * Componente responsável pela exibição da barra lateral (sidebar).
+ * Exibe opções de navegação baseadas no estado de autenticação e permissões do utilizador.
+ */
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -11,16 +15,32 @@ import { AuthService } from '../../../..//services/auth.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  
+  /**
+   * Serviço de autenticação utilizado para verificar o estado de login e permissões do utilizador.
+   */
   private authService = inject(AuthService);
 
+  /**
+   * Verifica se o utilizador está autenticado.
+   * Retorna um valor booleano que indica se o utilizador está autenticado ou não.
+   */
   get isAuthenticated(): boolean {
     return this.authService.isAuthenticated();
   }
 
+  /**
+   * Verifica se o utilizador tem permissões de administrador.
+   * Retorna um valor booleano que indica se o utilizador é um administrador.
+   */
   get isAdmin(): boolean {
     return this.authService.isAdmin();
   }
 
+  /**
+   * Verifica se o utilizador está associado a uma equipa.
+   * Retorna um valor booleano que indica se o utilizador tem uma equipa associada.
+   */
   get hasTeam(): boolean {
     return this.authService.hasTeam();
   }
