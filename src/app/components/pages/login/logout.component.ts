46a39b30b0756517a53fc5ee97cc96313973ccdb
8230148 <8230148@estg.ipp.pt>
2025-11-26 12:07:26 +0000

    documentacao logout

--- src/app/components/pages/login/logout.component.ts
+++ src/app/components/pages/login/logout.component.ts
@@ -3,21 +3,27 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { CookieService } from 'ngx-cookie-service';
 
+/**
+ * Componente responsável pelo logout do utilizador.
+ * Limpa os cookies de autenticação e redireciona para a página de login.
+ */
 @Component({
   selector: 'app-logout',
   template: '',
 })
 export class LogoutComponent {
   private cookieService = inject(CookieService);
-  
+
   constructor(private authService: AuthService, private router: Router) {
     this.logout();
   }
 
+  /**
+   * Realiza o logout do utilizador, excluindo os cookies de autenticação e redirecionando para a página de login.
+   */
   logout(): void {
     this.cookieService.delete('access_token', '/');
     this.cookieService.delete('user_id', '/');
-
     this.router.navigate(['/login']);
   }
 }
