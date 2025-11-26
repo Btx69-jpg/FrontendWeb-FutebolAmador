98a370587886ac36cae81822786d1b152f5c456b
8230148 <8230148@estg.ipp.pt>
2025-11-26 14:30:26 +0000

    documentacao auth guard

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -3,16 +3,34 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs';
 import { AuthService } from '../..//services/auth.service';
 
+/**
+ * Guarda de autenticação responsável por proteger rotas com base em permissões de autenticação e autorização.
+ * Verifica se o utilizador está autenticado e se possui as permissões necessárias para acessar a rota.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+
+  /**
+   * Serviço de autenticação, utilizado para verificar se o utilizador está autenticado,
+   * se ele é um administrador, ou se está associado a uma equipa.
+   */
   constructor(private authService: AuthService, private router: Router) {}
 
+  /**
+   * Método que determina se a navegação para uma rota específica pode ocorrer.
+   * Verifica a autenticidade do utilizador, permissões de administrador e se o utilizador tem uma equipa associada.
+   * Caso alguma dessas verificações falhe, o utilizador será redirecionado para a página de login ou outra página apropriada.
+   * @param route A rota ativada que está sendo verificada.
+   * @param state O estado da rota, que contém informações sobre a navegação.
+   * @returns Um valor booleano ou um Observable/Promise que determina se a navegação pode continuar.
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+
     const isAuthenticated = this.authService.isAuthenticated();
 
     if (!isAuthenticated) {
