a8601f31ea3c4aa71341ab6a0c2703a469e8bcd8
8230148 <8230148@estg.ipp.pt>
2025-11-26 14:12:33 +0000

    documentacao environments

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,4 +1,18 @@
+/**
+ * Ambiente de configuração para o desenvolvimento.
+ * Define variáveis específicas para o ambiente de desenvolvimento, como o estado de produção e a URL da API.
+ */
 export const environment = {
+  
+  /**
+   * Define se o ambiente é de produção ou desenvolvimento.
+   * Neste caso, está definido como `false`, indicando que é o ambiente de desenvolvimento.
+   */
   production: false,
+
+  /**
+   * A URL base para a API do backend.
+   * A URL foi configurada para um servidor local durante o desenvolvimento.
+   */
   apiBaseUrl: 'http://localhost:5218/api',
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,18 @@
+/**
+ * Ambiente de configuração para a produção.
+ * Define variáveis específicas para o ambiente de produção, como o estado de produção e a URL da API.
+ */
 export const environment = {
+  
+  /**
+   * Define se o ambiente é de produção ou desenvolvimento.
+   * Neste caso, está definido como `true`, indicando que é o ambiente de produção.
+   */
   production: true,
+
+  /**
+   * A URL base para a API do backend.
+   * A URL foi configurada para um servidor local durante a produção.
+   */
   apiBaseUrl: 'http://localhost:5218/api',
-};
+}
