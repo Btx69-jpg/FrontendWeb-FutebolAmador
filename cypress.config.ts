d9244ccdb967ad3a794c9d96ade482fac02626ea
Willkie <8230127@estg.ipp.pt>
2025-12-05 18:12:26 +0000

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   
   e2e: {
+    setupNodeEvents(on, config) {},
+    video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     'baseUrl': 'http://localhost:4200'
   },
   
